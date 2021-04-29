42a011c97b5fff3b5cca76a83a02fbee6e0c5076
Anmol Chaudhary <chaudharyanmol321@gmail.com>
2021-04-29 10:00:04 +0530

    React+Fabric Demo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import FabricComp from './components/FabricComp'
+
+function App() {
+  return (
+    <div className='App'>
+      <FabricComp />
+    </div>
+  )
+}
+
+export default App
